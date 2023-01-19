93829ccb834bbc2c2f464c06e827e29561fbd96a
sneg333 <igortot@yandex.ru>
2023-01-19 17:31:38 +0300

    ava_flask no bd

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '112244'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ava.db'
+db = SQLAlchemy(app)
+
+class New(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable = False)
+    mini_text = db.Column(db.String(300), nullable = False)
+    text = db.Column(db.Text, nullable = False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<New %r>' % self.id
 
 menu =[
         {"name": "Домашняя", "url": "/"},
         {"name": "Контакты", "url": "contact"},
         {"name": "Добавить поcт", "url": "createpost"},
+        {"name": "новости", "url": "news"},
+        {"name": "вход", "url": "login"},
     ]
 
 @app.route('/')
@@ -17,6 +34,21 @@ def home():
 def contact():
     print(url_for('contact'))
     return render_template('contact.html', title="Контакты", menu=menu)
+
+@app.route('/login')
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.form['username'] == "selfedu" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="авторизация", menu=menu)
+
+@app.route('/news')
+def news():
+    print(url_for('news'))
+    return render_template('news.html', title="Новости", menu=menu)
 
 @app.route('/createpost', methods=["POST", "GET"])
 def createpost():
--- readme.txt
+++ readme.txt
@@ -1,5 +1,12 @@
 1 flash - сообщения после выполнения действий, например кведомление о том, 
     что покупка состоялась, или произошла ошибка
 2 request - обработка формы с методом GET или POST
-3 url_for
-4 render_template рендерить html файл со статикой
+3 url_for - подключение статики
+4 render_template рендерить html файл 
+5 from flask_sqlalchemy import SQLAlchemy - библиотека для работы с базой данных
+
+6 создание базы данных:
+    python3 
+        from app import db
+          db.create_all() 
+          exit() 
